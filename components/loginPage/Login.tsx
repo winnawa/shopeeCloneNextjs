d8cd4ce21582fb658ecbfa5f405b7465df5d308c
Nam Khoa <khoa.pham28042002@hcmut.edu.vn>
2022-09-03 21:13:45 +0700

    fix bug related to parallax at index 1

--- components/loginPage/Login.tsx
+++ components/loginPage/Login.tsx
@@ -1,7 +1,7 @@
 import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth"
 import { app } from "../../src/firebase/firebase"
 import React, { useEffect, useState } from "react";
-import {Parallax,ParallaxLayer} from "@react-spring/parallax"
+//import {Parallax,ParallaxLayer} from "@react-spring/parallax"
 import { Button, FlexContainer, ImgContainer, TextBackGroundAndSizeColorBox, TextCenterBox, TextContainer } from "./styled";
 import { useRouter } from "next/router";
 const LoginComponent = ()=>{
